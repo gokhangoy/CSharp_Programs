d0a3222ad54a672e98353c72ed62a2acd142bc74
goygokhan <bioinformaticiangokhangoy@gmail.com>
2021-05-23 23:14:02 +0300

    Generics_Introduction

---
+++ Generics_Introduction/MyList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics_Introduction
+{
+    class MyList<T>
+    {
+        T[] items;       //In this class we need an array to store items. Type of the elements could be different.
+        public MyList()
+        {
+            items = new T[0];  //Line 11 in Program.cs
+        }
+
+        public void AddItem(T item)
+        {
+            T[] tempArray = items;  //We are not going to loose the old reference for items so we are not going to loose old values...
+            items = new T[items.Length + 1];
+
+            for (int i = 0; i < items.Length-1; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
+        }
+
+        public int GetLength()
+        {
+            return items.Length;
+        }
+
+        public void DisplayList()
+        {
+            foreach (var item in items)
+            {
+                Console.Write(item+"\t");
+            }
+        }
+    }
+}
---
+++ Generics_Introduction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics_Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> numbers = new MyList<int> {};
+            // Console.WriteLine(numbers.Count); At the beginnig we have a list which contains zero element.
+
+
+            Console.WriteLine("\nThe Length: " + numbers.GetLength());
+            numbers.AddItem(12);
+            numbers.DisplayList();
+            Console.WriteLine("\nThe Length: "+numbers.GetLength());
+            numbers.AddItem(25);
+            numbers.DisplayList();
+            Console.WriteLine("\nThe Length: " + numbers.GetLength());
+            numbers.AddItem(36);
+            numbers.DisplayList();
+            Console.WriteLine("\nThe Length: " + numbers.GetLength());
+            numbers.AddItem(52);
+            numbers.DisplayList();
+            Console.WriteLine("\nThe Length: " + numbers.GetLength());
+            numbers.AddItem(536);
+            numbers.DisplayList();
+            Console.WriteLine("\nThe Length: " + numbers.GetLength());
+
+        }
+    }
+}
